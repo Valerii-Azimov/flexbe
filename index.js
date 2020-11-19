111544d03bab5ec53d0f46e27d0344e9d742a2f3
unknown <azimov3.9.1991@gmail.com>
2020-11-19 17:01:56 +0200

    3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const refs = {
   burgerButton: document.querySelector('button[data-action="burger-button"]'),
   modal: document.querySelector("#main-modal"),
+  mainPage: document.querySelector("#main-page"),
 };
 
 refs.burgerButton.addEventListener("click", eventModal);
@@ -8,7 +9,9 @@ refs.burgerButton.addEventListener("click", eventModal);
 function eventModal() {
   if (refs.modal.classList.contains("modal--hide")) {
     refs.modal.classList.remove("modal--hide");
+    refs.mainPage.classList.add("modal--hide");
   } else {
     refs.modal.classList.add("modal--hide");
+    refs.mainPage.classList.remove("modal--hide");
   }
 }
